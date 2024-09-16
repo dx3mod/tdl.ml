47f9c9ede90084ca2cef9429f85a35070db6a80a
dx3mod <dx3mod@bk.ru>
2024-09-16 17:07:31 +0300

    Add minimal API set

--- lib/function_description.ml
+++ lib/function_description.ml
@@ -5,4 +5,23 @@ module Functions (F : Ctypes.FOREIGN) = struct
   open F
 
   let get_version = foreign "tdl_get_version" (void @-> returning int)
+  let canvas = foreign "tdl_canvas" (void @-> returning (ptr Types.canvas))
+
+  let destroy_canvas =
+    foreign "tdl_destroy_canvas" (ptr Types.canvas @-> returning void)
+
+  let u8string = foreign "u8string" (string @-> returning Types.U8string.t)
+
+  let style =
+    foreign "tdl_style"
+      (Types.Style.Point_color.t @-> int @-> returning Types.Style.t)
+
+  let text =
+    foreign "tdl_text"
+      (Types.U8string.t @-> Types.Style.t @-> returning Types.Text.t)
+
+  let print =
+    foreign "tdl_print" (ptr Types.canvas @-> Types.Text.t @-> returning bool)
+
+  let display = foreign "tdl_display" (ptr Types.canvas @-> returning void)
 end
--- lib/type_description.ml
+++ lib/type_description.ml
@@ -1,1 +1,42 @@
-module Types (F : Ctypes.TYPE) = struct end
+open Ctypes
+
+module Types (F : Ctypes.TYPE) = struct
+  type canvas
+  and text
+  and style
+  and u8string
+  and point_color
+
+  let canvas : canvas structure typ = structure "tdl_canvas"
+  let style : style structure typ = structure "tdl_style"
+
+  module U8string = struct
+    let t : u8string structure typ = structure "u8string"
+    let string = field t "string" (ptr char)
+    let length = field t "length" int
+    let () = seal t
+  end
+
+  module Style = struct
+    let t : style structure typ = structure "tdl_style"
+
+    module Point_color = struct
+      let color = uint
+      let t : point_color structure typ = structure "tdl_point_color"
+      let bg = field t "bg" color
+      let fg = field t "fg" color
+      let () = seal t
+    end
+
+    let attributes = field t "attributes" int
+    let color = field t "color" Point_color.t
+    let () = seal t
+  end
+
+  module Text = struct
+    let t : text structure typ = structure "tdl_text"
+    let string = field t "string" U8string.t
+    let style = field t "style" Style.t
+    let () = seal t
+  end
+end
